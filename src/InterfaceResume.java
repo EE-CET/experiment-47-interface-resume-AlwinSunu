e803c86d63f554c2ebbdbe3788fd3fa2868dca3e
Alwin Sunu <alwinsunu1974@gmail.com>
2026-02-13 10:40:07 +0530

    Update InterfaceResume.java

--- src/InterfaceResume.java
+++ src/InterfaceResume.java
@@ -1,19 +1,35 @@
 import java.util.Scanner;
 
 interface Resume {
+    void biodata();
+    
     // TODO: Declare method void biodata();
 }
 
 class Teacher implements Resume {
     // Attributes
-   
+   String name;
+   String Qualification;
+   String Experience;
+    
+void biodata(){
 
+    
+}
     // TODO: Implement biodata() method to print details in the required format
 }
 
 public class InterfaceResume {
    
-        
+        public void main(String args[]){
+
+            Scanner sc=new Scanner(System.in);
+            int name=sc.nextLine();
+            int qual=sc.nextLine();
+            int exp=sc.nextLine();   
+
+            
+        }
         // TODO: Read input (Name, Qualification, Experience)
         // Assign to teacher object attributes
         
